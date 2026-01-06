b39d2c36c77b4dd8412f5ff60ca97d9ed001991e
Jack Griffiths Rico <j.griffithsrico@gmail.com>
2026-01-06 11:19:55 +0000

    added socket listen

--- c/http-server/http-server.c
+++ c/http-server/http-server.c
@@ -4,32 +4,55 @@
 #include <arpa/inet.h>
 
 int main(void){
-    int tcp_socket = socket(
-        AF_INET,
-        SOCK_STREAM,
-        0
-    );
+	int rc = 0;
+    struct sockaddr_in bind_addr;
+	int tcp_socket = 0;
+	int client_socket = 0;
 
-    if (tcp_socket == -1){
+    memset(&bind_addr, 0, sizeof(bind_addr));
+	tcp_socket = socket(
+		AF_INET,
+		SOCK_STREAM,
+		0
+	);
+
+    if (tcp_socket < 0){
         perror("socket()");
         return 1;
     }
     printf("socket creation succeeded\n");
 
-    struct sockaddr_in bind_addr;
-    memset(&bind_addr, 0, sizeof(bind_addr));
     bind_addr.sin_port = htons(9999);
     bind_addr.sin_family = AF_INET;
     bind_addr.sin_addr.s_addr = INADDR_ANY;
 
-    int rc = bind(
+   	rc = bind(
         tcp_socket,
         (const struct sockaddr*)&bind_addr,
         sizeof(bind_addr)
     );
     if (rc < 0) {
         perror("bind()");
+		close(tcp_socket);
         return 1;
     }
     printf("bind succeeded\n");
+
+    rc = listen(
+		tcp_socket,
+		SOMAXCONN
+		);
+	if (rc < 0){
+		perror("listen()");
+		close(tcp_socket);
+		return 1;
+	}
+	printf("listen succeeded\n");
+
+	printf("waiting for connections\n");
+	client_socket = accept(tcp_socket, NULL, NULL);
+
+	printf("got a connection\n");
+
+	return 0;
 }
