48ef424cc82ef4c120e08a237a4b1d598cdf8e14
Jack Griffiths Rico <j.griffithsrico@gmail.com>
2026-01-13 00:54:20 +0000

    cpp server can handle connections and handle/parse requests

--- cpp/http-server/http_server.cpp
+++ cpp/http-server/http_server.cpp
@@ -4,6 +4,30 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#define CRLF "\r\n"
+#define SP " "
+
+typedef struct
+{
+    std::string method;
+    std::string uri;
+    std::string version;
+} http_request_line;
+
+typedef enum
+{
+    HTTP_RES_OK = 200,
+    HTTP_RES_BAD_REQUEST = 400,
+    HTTP_RES_NOT_FOUND = 404,
+    HTTP_RES_INTERNAL_SERVER_ERR = 500,
+} http_status_code;
+
+int handle_connection(int socket);
+int handle_request(int socket);
+http_status_code parse_request(const std::string& request, http_request_line& request_line);
+std::string generate_response_header(std::string& request);
+int handle_response(int socket, std::string& response_header);
+
 int main()
 {
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -47,15 +71,81 @@ int main()
     }
     std::cout << "listen successful" << std::endl;
 
-	for (;;)
-	{
+    for (;;)
+    {
         std::cout << "waiting for connections..." << std::endl;
-        
-		int clientSocket = accept(serverSocket, nullptr, nullptr);
+
+        int clientSocket = accept(serverSocket, nullptr, nullptr);
         std::cout << "got a connection" << std::endl;
 
-        // rc = handle_connection(clientSocket);
-	}
+        rc = handle_connection(clientSocket);
+        if (rc < 0)
+        {
+            std::cerr << "handle connection failed" << std::endl;
+        }
+    }
 
     return 0;
+}
+
+int handle_connection(int socket)
+{
+    char request_buffer[1024] = {0};
+    ssize_t rc = 0;
+
+    rc = recv(socket, request_buffer, sizeof(request_buffer), 0);
+    if (rc < 0)
+    {
+        std::cerr << "recv()" << std::endl;
+        close(socket);
+        return -1;
+    }
+    request_buffer[rc] = '\0';
+
+    http_request_line request_line;
+    request_line.method = "";
+    request_line.uri = "";
+    request_line.version = "";
+
+    std::string request(request_buffer);
+    http_status_code status = parse_request(request, request_line);
+    if (status != HTTP_RES_OK)
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
+http_status_code parse_request(const std::string& request, http_request_line& request_line)
+{
+    if (request.empty())
+    {
+        std::cout << "parse request failed" << std::endl;
+        return HTTP_RES_INTERNAL_SERVER_ERR;
+    }
+
+    size_t line_end = request.find(CRLF);
+    if (line_end == std::string::npos)
+    {
+        std::cout << "bad request" << std::endl;
+        return HTTP_RES_BAD_REQUEST;
+    }
+
+    std::string line = request.substr(0, line_end);
+
+    size_t method_end = line.find(SP);
+    size_t uri_end = line.find(SP, method_end + 1);
+
+    if (method_end == std::string::npos || uri_end == std::string::npos)
+    {
+        std::cout << "bad request" << std::endl;
+        return HTTP_RES_BAD_REQUEST;
+    }
+
+    request_line.method = line.substr(0, method_end);
+    request_line.uri = line.substr(method_end + 1, uri_end - (method_end + 1));
+    request_line.version = line.substr(uri_end + 1);
+
+    return HTTP_RES_OK;
 }
